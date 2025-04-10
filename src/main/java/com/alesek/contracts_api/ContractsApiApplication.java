b23796128621e87b899df59210edc29505d32ba3
Alesek <acapek@email.cz>
2025-04-10 20:06:36 +0200

    init commit

---
+++ src/main/java/com/alesek/contracts_api/ContractsApiApplication.java
@@ -0,0 +1,13 @@
+package com.alesek.contracts_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractsApiApplication.class, args);
+	}
+
+}
